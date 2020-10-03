2b1b628ae4f391fecded2f0dfd08e87a7a9b67a3
Jeka95 <lincspeed95@gmail.com>
2020-10-03 05:41:42 +0300

    19

--- JS/main.js
+++ JS/main.js
@@ -1,126 +1,63 @@
-let submit = document.querySelector(".submit");
-submit.addEventListener("click", ValidMail,);
-submit.addEventListener("click", ValidFirstName);
-submit.addEventListener("click", ValidLastName);
-submit.addEventListener("click", ValidPassword);
-submit.addEventListener("click", ValidTel);
-document.addEventListener("input", checkInput)
-let input = document.querySelectorAll(".inputText");
-let myMail = document.querySelector(".email");
-let emailerror = document.querySelector(".emailerror");
-let nameerror = document.querySelector(".nameeror");
-let lastnameeror = document.querySelector(".lastnameeror");
-let placehold = document.querySelectorAll(".placehold");
-let firstName = document.querySelector(".firsname");
-let lastName = document.querySelector(".lastname");
-let password = document.querySelector(".password");
-let passwerror = document.querySelector(".passwerror");
-let phone = document.querySelector(".phone");
-let phoneeror = document.querySelector(".phoneeror")
-function checkInput() {
-  for (let i = 0; i < input.length; i++) {
-    if (input[i].value == "") {
-      placehold[i].classList.remove("active");
-    } else {
-      placehold[i].classList.add("active");
-    }
-  }
-}
-function ValidMail() {
-  let regmail = /^[\w-\.]+@[\w-]+\.[a-z]{2,4}$/i;
-  let valid = regmail.test(myMail.value);
-  if (valid) {
-    myMail.classList.add("greenok");
-    emailerror.style.opacity = 0;
-    myMail.classList.remove("redno");
-  }
-  else {
-    emailerror.style.opacity = 1;
-    myMail.classList.add("redno");
-    myMail.classList.remove("greenok");
-    checkInput();
-  }
-  return valid;
-}
-function ValidFirstName() {
-  let regName = /^[a-zA-Z]{2,20}$/i;
-  let valid = regName.test(firstName.value);
-  if (valid) {
-    firstName.classList.add("greenok");
-    nameerror.style.opacity = 0;
-    firstName.classList.remove("redno");
-  }
-  else {
-    nameerror.style.opacity = 1;
-    firstName.classList.add("redno");
-    firstName.classList.remove("greenok");
-    checkInput();
-  }
-}
-function ValidLastName() {
-  let regName = /^[a-zA-Z]{2,20}$/i;
-  let valid = regName.test(lastName.value);
-  if (valid) {
-    lastName.classList.add("greenok");
-    lastnameeror.style.opacity = 0;
-    lastName.classList.remove("redno");
-  }
-  else {
-    lastnameeror.style.opacity = 1;
-    lastName.classList.add("redno");
-    lastName.classList.remove("greenok");
-    checkInput();
-  }
-}
-
-function ValidPassword() {
-  let regName = /^[a-zA-Z0-9]{8,15}$/i;
-  let valid = regName.test(password.value);
-  if (valid) {
-    password.classList.add("greenok");
-    passwerror.style.opacity = 0;
-    password.classList.remove("redno");
-  }
-  else {
-    passwerror.style.opacity = 1;
-    password.classList.add("redno");
-    password.classList.remove("greenok");
-    checkInput();
-  }
-}
-function ValidTel() {
-  if (phone.value.length == 18) {
-    phone.classList.add("greenok");
-    phoneeror.style.opacity = 0;
-    phone.classList.remove("redno");
-  }
-  else {
-    phoneeror.style.opacity = 1;
-    phone.classList.add("redno");
-    phone.classList.remove("greenok");
-    checkInput();
-  }
-}
-console.log(phone.value.length);
-
-
-
-window.addEventListener("DOMContentLoaded", function () {
-
-  function mask(event) {
-    let matrix = "+38(0__)-__-__-___",
-      i = 0,
-      def = matrix.replace(/\D/g, ""),
-      val = this.value.replace(/\D/g, "");
-    console.log("def: " + def);
-    console.log("val: " + val);
-    if (def.length >= val.length) val = def;
-    this.value = matrix.replace(/./g, function (a) {
-      return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? "" : a
-    });
-  };
-  let input = document.querySelector(".phone");
-  input.addEventListener("input", mask, false);
-  input.addEventListener("focus", mask, false);
-
+let deleteuser;
+window.addEventListener("input", function (event) {
+  inputChecker(event);
 });
+let adduser = document.querySelector(".btn")
+adduser.addEventListener("click", addUser);
+function inputChecker(event) {
+  let type = {
+    text: /^[A-Za-z]{4,16}$/,
+    password: /^[A-Za-z0-9\_]{4,16}$/,
+    email: /^[A-Za-z]{2,20}@[A-Za-z]{1,8}\.[A-Za-z]{2,3}$/
+  };
+  if (type[event.target.type].test(event.target.value)) {
+    console.log(type[event.target.type].test(event.target.value));
+    event.target.style.border = "4px green solid";
+    adduser.removeAttribute("disabled", "disabled");
+  } else {
+    event.target.style.border = "4px red solid";
+    adduser.setAttribute("disabled", "disabled");
+  }
+}
+let personlist = [];
+function addUser() {
+  let person = {
+    login: document.querySelector(".login_input").value,
+    password: document.querySelector(".paswword_input").value,
+    email: document.querySelector(".email_input").value,
+  };
+  document.querySelector(".login_input").value = "";
+  document.querySelector(".paswword_input").value = "";
+  document.querySelector(".email_input").value = "";
+  personlist.push(person);
+  table.innerHTML = "";
+  render(table, personlist);
+}
+
+let table = document.querySelector("#table")
+function render(table, arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let tr = document.createElement('tr');
+    let td1 = document.createElement('td');
+    td1.className = "number";
+    td1.innerHTML = i + 1;
+    tr.appendChild(td1);
+    for (let key in arr[i]) {
+      let td = document.createElement('td');
+      td.className = "input_list";
+      td.innerHTML = arr[i][key];
+      tr.appendChild(td);
+    }
+    let td5 = document.createElement('td');
+    td5.className = "edit_list"
+    let td6 = document.createElement('td');
+    td6.className = "delete_list";
+    td5.innerHTML = "<input type='button' class='edit' value='Edit'> ";
+    td5.id = "indexedit" + `${i + 1}`;
+    td6.innerHTML = "<input type='button' class='delete' value='Delete'>";
+    td6.id = "indexdelete" + `${i + 1}`;
+    tr.appendChild(td5);
+    tr.appendChild(td6);
+    table.appendChild(tr);
+  }
+}
